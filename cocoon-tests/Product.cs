63571825cee5a13069e181edaf054c2a21828be6
Rhett Thompson <rhett.thompson@live.com>
2015-05-18 09:43:21 -0400

    renamed

---
+++ cocoon-tests/Product.cs
@@ -0,0 +1,25 @@
+﻿using Cocoon.Annotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cocoon.Tests
+{
+
+    [Table]
+    class Product
+    {
+
+        [Column, PrimaryKey, IgnoreOnUpdate]
+        public Guid ProductID { get; set; }
+
+        [Column, NotNull]
+        public string ProductName { get; set; }
+
+        [Column(DefaultValue: "getutcdate()"), IgnoreOnInsert, IgnoreOnUpdate, NotNull]
+        public DateTime CreateDate { get; set; }
+
+    }
+}
