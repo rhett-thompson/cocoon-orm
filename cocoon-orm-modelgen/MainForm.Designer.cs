455a1685d6e08efb6f3a60e8f5cbaace98970987
Rhett Thompson <rhett.thompson@live.com>
2016-03-30 15:57:30 -0400

    updates

--- cocoon-orm-modelgen/MainForm.Designer.cs
+++ cocoon-orm-modelgen/MainForm.Designer.cs
@@ -133,9 +133,9 @@
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(12, 260);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(108, 19);
+            this.label3.Size = new System.Drawing.Size(121, 19);
             this.label3.TabIndex = 6;
-            this.label3.Text = "Generated Class";
+            this.label3.Text = "Generated Classes";
             // 
             // SelectAllButton
             // 
