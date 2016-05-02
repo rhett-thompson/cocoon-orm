f2584e4c124979c3d913de99c49fb9bc65031553
Rhett Thompson <rhett.thompson@live.com>
2016-05-02 10:08:43 -0400

    added PredicateBuilder class for building dynamic where clauses

---
+++ cocoon-orm/PredicateBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Cocoon.ORM
+{
+    public static class PredicateBuilder
+    {
+        public static Expression<Func<T, bool>> Or<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+        {
+            InvocationExpression invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>(Expression.OrElse(expr1.Body, invokedExpr), expr1.Parameters);
+        }
+
+        public static Expression<Func<T, bool>> And<T>(this Expression<Func<T, bool>> expr1, Expression<Func<T, bool>> expr2)
+        {
+            InvocationExpression invokedExpr = Expression.Invoke(expr2, expr1.Parameters.Cast<Expression>());
+            return Expression.Lambda<Func<T, bool>>(Expression.AndAlso(expr1.Body, invokedExpr), expr1.Parameters);
+        }
+    }
+}
